a4ca4494344c45c5b374d99e219a1cdd7f565b34
emmanyouwell <emingala02@gmail.com>
2024-05-01 11:09:25 +0800

    prevent duplicate restrictions

--- admin/manage_restriction.php
+++ admin/manage_restriction.php
@@ -1,5 +1,6 @@
 <?php
 include '../db_connect.php';
+
 ?>
 <div class="container-fluid">
 	<form action="" id="manage-restriction">
@@ -11,13 +12,13 @@ include '../db_connect.php';
 					<label for="" class="control-label">Faculty</label>
 					<select name="" id="faculty_id" class="form-control form-control-sm select2">
 						<option value=""></option>
-						<?php 
+						<?php
 						$faculty = $conn->query("SELECT *,concat(firstname,' ',lastname) as name FROM faculty_list order by concat(firstname,' ',lastname) asc");
 						$f_arr = array();
-						while($row=$faculty->fetch_assoc()):
-							$f_arr[$row['id']]= $row;
-						?>
-						<option value="<?php echo $row['id'] ?>" <?php echo isset($faculty_id) && $faculty_id == $row['id'] ? "selected" : "" ?>><?php echo ucwords($row['name']) ?></option>
+						while ($row = $faculty->fetch_assoc()):
+							$f_arr[$row['id']] = $row;
+							?>
+							<option value="<?php echo $row['id'] ?>" <?php echo isset($faculty_id) && $faculty_id == $row['id'] ? "selected" : "" ?>><?php echo ucwords($row['name']) ?></option>
 						<?php endwhile; ?>
 					</select>
 				</div>
@@ -25,20 +26,21 @@ include '../db_connect.php';
 					<label for="" class="control-label">Class</label>
 					<select name="" id="class_id" class="form-control form-control-sm select2">
 						<option value=""></option>
-						<?php 
+						<?php
 						$classes = $conn->query("SELECT id,concat(curriculum,' ',level,' - ',section) as class FROM class_list");
 						$c_arr = array();
-						while($row=$classes->fetch_assoc()):
-							$c_arr[$row['id']]= $row;
-						?>
-						<option value="<?php echo $row['id'] ?>" <?php echo isset($class_id) && $class_id == $row['id'] ? "selected" : "" ?>><?php echo $row['class'] ?></option>
+						while ($row = $classes->fetch_assoc()):
+							$c_arr[$row['id']] = $row;
+							?>
+							<option value="<?php echo $row['id'] ?>" <?php echo isset($class_id) && $class_id == $row['id'] ? "selected" : "" ?>><?php echo $row['class'] ?></option>
 						<?php endwhile; ?>
 					</select>
 				</div>
-				
+
 				<div class="form-group">
 					<div class="d-flex w-100 justify-content-center">
-						<button class="btn btn-sm btn-flat btn-primary bg-gradient-primary" id="add_to_list" type="button">Add to List</button>
+						<button class="btn btn-sm btn-flat btn-primary bg-gradient-primary" id="add_to_list"
+							type="button">Add to List</button>
 					</div>
 				</div>
 			</div>
@@ -48,31 +50,32 @@ include '../db_connect.php';
 						<tr>
 							<th>Faculty</th>
 							<th>Class</th>
-							
+
 							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody>
-						<?php 
+						<?php
 						$restriction = $conn->query("SELECT * FROM restriction_list where academic_id = {$_GET['id']} order by id asc");
-						while($row=$restriction->fetch_assoc()):
-						?>
-						<tr>
-							<td>
-								<b><?php echo isset($f_arr[$row['faculty_id']]) ? $f_arr[$row['faculty_id']]['name'] : '' ?></b>
-								<input type="hidden" name="rid[]" value="<?php echo $row['id'] ?>">
-								<input type="hidden" name="faculty_id[]" value="<?php echo $row['faculty_id'] ?>">
-							</td>
-							<td>
-								<b><?php echo isset($c_arr[$row['class_id']]) ? $c_arr[$row['class_id']]['class'] : '' ?></b>
-								<input type="hidden" name="class_id[]" value="<?php echo $row['class_id'] ?>">
-							</td>
-							
-							<td class="text-center">
-								<button class="btn btn-sm btn-outline-danger" onclick="$(this).closest('tr').remove()" type="button"><i class="fa fa-trash"></i></button>
-							</td>
-						</tr>
-					<?php endwhile; ?>
+						while ($row = $restriction->fetch_assoc()):
+							?>
+							<tr>
+								<td>
+									<b><?php echo isset($f_arr[$row['faculty_id']]) ? $f_arr[$row['faculty_id']]['name'] : '' ?></b>
+									<input type="hidden" name="rid[]" value="<?php echo $row['id'] ?>">
+									<input type="hidden" name="faculty_id[]" value="<?php echo $row['faculty_id'] ?>">
+								</td>
+								<td>
+									<b><?php echo isset($c_arr[$row['class_id']]) ? $c_arr[$row['class_id']]['class'] : '' ?></b>
+									<input type="hidden" name="class_id[]" value="<?php echo $row['class_id'] ?>">
+								</td>
+
+								<td class="text-center">
+									<button class="btn btn-sm btn-outline-danger" onclick="$(this).closest('tr').remove()"
+										type="button"><i class="fa fa-trash"></i></button>
+								</td>
+							</tr>
+						<?php endwhile; ?>
 					</tbody>
 				</table>
 			</div>
@@ -80,50 +83,65 @@ include '../db_connect.php';
 	</form>
 </div>
 <script>
-	$(document).ready(function(){
+	$(document).ready(function () {
 		$('.select2').select2({
-		    placeholder:"Please select here",
-		    width: "100%"
-		  });
-		$('#manage-restriction').submit(function(e){
+			placeholder: "Please select here",
+			width: "100%"
+		});
+		$('#manage-restriction').submit(function (e) {
 			e.preventDefault();
 			start_load()
 			$('#msg').html('')
 			$.ajax({
-				url:'ajax.php?action=save_restriction',
-				method:'POST',
-				data:$(this).serialize(),
-				success:function(resp){
-					if(resp == 1){
-						alert_toast("Data successfully saved.","success");
-						setTimeout(function(){
-							location.reload()	
-						},1750)
-					}else if(resp == 2){
+				url: 'ajax.php?action=save_restriction',
+				method: 'POST',
+				data: $(this).serialize(),
+				success: function (resp) {
+					if (resp == 1) {
+						alert_toast("Data successfully saved.", "success");
+						setTimeout(function () {
+							location.reload()
+						}, 1750)
+					} else if (resp == 2) {
 						$('#msg').html('<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> Class already exist.</div>')
 						end_load()
 					}
 				}
 			})
 		})
-		$('#add_to_list').click(function(){
+		$('#add_to_list').click(function () {
 			start_load()
 			var frm = $('#manage-restriction')
 			var cid = frm.find('#class_id').val()
 			var fid = frm.find('#faculty_id').val()
-			
+
 			var f_arr = <?php echo json_encode($f_arr) ?>;
 			var c_arr = <?php echo json_encode($c_arr) ?>;
-			
+			// Check if the selected class_id and faculty_id are already in the list
+			var isDuplicate = false;
+			$('#r-list tbody tr').each(function () {
+				var rowFid = $(this).find('input[name="faculty_id[]"]').val();
+				var rowCid = $(this).find('input[name="class_id[]"]').val();
+				if (rowFid == fid && rowCid == cid) {
+					isDuplicate = true;
+					return false; // break the loop
+				}
+			});
+
+			if (isDuplicate) {
+				alert_toast("Data already exist in the list.", "warning")
+				end_load()
+				return;
+			}
 			var tr = $("<tr></tr>")
-			tr.append('<td><b>'+f_arr[fid].name+'</b><input type="hidden" name="rid[]" value=""><input type="hidden" name="faculty_id[]" value="'+fid+'"></td>')
-			tr.append('<td><b>'+c_arr[cid].class+'</b><input type="hidden" name="class_id[]" value="'+cid+'"></td>')
-			
+			tr.append('<td><b>' + f_arr[fid].name + '</b><input type="hidden" name="rid[]" value=""><input type="hidden" name="faculty_id[]" value="' + fid + '"></td>')
+			tr.append('<td><b>' + c_arr[cid].class + '</b><input type="hidden" name="class_id[]" value="' + cid + '"></td>')
+
 			tr.append('<td class="text-center"><span class="btn btn-sm btn-outline-danger" onclick="$(this).closest(\'tr\').remove()" type="button"><i class="fa fa-trash"></i></span></td>')
 			$('#r-list tbody').append(tr)
 			frm.find('#class_id').val('').trigger('change')
 			frm.find('#faculty_id').val('').trigger('change')
-			
+
 			end_load()
 		})
 	})
